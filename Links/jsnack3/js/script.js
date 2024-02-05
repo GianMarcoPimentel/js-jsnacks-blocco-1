86dca1e8814d0d4d5ed153bf84c1e360d21b008f
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-05 10:25:48 +0100

    Perfezionamento SNACK-3

--- Links/jsnack3/js/script.js
+++ Links/jsnack3/js/script.js
@@ -6,13 +6,20 @@ Solo se è dispari inseriscilo nell’array.
 Stampa a schermo il contenuto dell'array
 */
 
+//Crea un array vuoto.
 const numbers = [];
 
+
+//Chiedi per 6 volte all’utente di inserire un numero,
 for(i = 0; i<6; i++){
     numeri = Number(prompt("Inserisci un numero"));
-    if(numeri % 2 != 0){
-        numbers.push(numeri);
+
+    //Solo se è dispari inseriscilo nell’array.
+        if(numeri % 2 != 0){
+        numbers.push(numeri); //Il push serve per appunto pushare ( spingere ) un elemento all'interno dell'array indicato!
         console.log(numbers)
     };
 }
+
+//Stampa a schermo il contenuto dell'array
 document.getElementById("array-finale").innerHTML = `L'array finale è composta da numbers = [${numbers}]`;
