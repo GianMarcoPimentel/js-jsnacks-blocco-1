5fe16eb9747f01e278fd4b0afafc2b1c4ff0b72c
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-06-06 17:53:33 +0200

    snack 3

--- Links/jsnack3/js/script.js
+++ Links/jsnack3/js/script.js
@@ -5,39 +5,18 @@ Chiedi per 6 volte all’utente di inserire un numero,
 Solo se è dispari inseriscilo nell’array.
 Stampa a schermo il contenuto dell'array
 */
-
-//Crea un array vuoto.
+//array vuoto
 const numbers = [];
 
-
-//Chiedi per 6 volte all’utente di inserire un numero,
-for(i = 0; i<6; i++){
-    numeri = Number(prompt("Inserisci un numero"));
-
-    //Solo se è dispari inseriscilo nell’array.
-        if(numeri == null || isNaN(numeri)){
-            alert("Ricarica la pagina eInserisci numeri che soddisfino la richiesta")
-        } else {
-            if(numeri % 2 != 0){
-            numbers.push(numeri); //Il push serve per appunto pushare ( spingere ) un elemento all'interno dell'array indicato!
-            console.log(numbers)
-              }
-        }
+for(i = 0; i < 6 ; i ++) {
+    number = Number(prompt('Inserisci un numero'));
+    if(number == null || isNaN(number)) {
+        alert('Devi inserire un n umero, ricarica la pagina');
+    }else if(number % 2 != 0){
+        numbers.push(number);
+       
+    }
+    
 }
 
-//Stampa a schermo il contenuto dell'array
-document.getElementById("array-finale").innerHTML = `L'array finale è composta da numbers = [${numbers}]`;
-
-document.querySelector("h2").innerHTML = "Ecco l'eleno del mio array messo in una lista :"
-const listElement = document.querySelector("ul");
-for( let i = 0; i < numbers.length; i++){
-
-
-        // creo un elemento html <li>
-        const newElement = document.createElement("li");
-
-        // ne cambio il testo interno
-        newElement.innerText = numbers[i];
-        // lo inserisco in pagina dentro al lista selezionta precedentemente
-        listElement.append(newElement);
-}
+document.getElementById('array-finale').innerHTML = `[${numbers}]`;
